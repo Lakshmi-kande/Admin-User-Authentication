0c66fd688dcca75f355b8a31b207852cead4ef04
Lakshmi-kande <kandelakshmi500@gmail.com>
2023-07-07 12:33:11 +0530

    changes on swagger

--- backend/swaggerApi.js
+++ backend/swaggerApi.js
@@ -173,6 +173,36 @@
  *         description: Validation error or user not found
  *       401:
  *         description: Unauthorized
+ * 
+ * 
+ * /api/dashboard/users:
+ *   get:
+ *     summary: Get all users
+ *     description: Use this endpoint to retrieve a list of all users
+ *     tags:
+ *       - Dashboard
+ *     responses:
+ *       200:
+ *         description: Successful response with a list of users
+ *       500:
+ *         description: Internal server error
+ *
+ * /api/dashboard/users/{userId}:
+ *   delete:
+ *     summary: Delete a user
+ *     description: Use this endpoint to delete a user by ID
+ *     tags:
+ *       - Dashboard
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         description: ID of the user to delete
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       500:
+ *         description: Internal server error
  */
-
-
